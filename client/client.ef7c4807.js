a7ca0da54cb5ab1087b59a4c71ff0d5ea0184179
Le Liam <nghenglim@gmail.com>
2020-01-02 10:33:48 +0800

    new year new format

---
+++ client/client.ef7c4807.js
@@ -0,0 +1,1 @@
+import"./index.0f7267ba.js";import{s as e}from"./app.900f9ceb.js";e({target:document.querySelector("#sapper")});
---
+++ client/legacy/client.994d7ee4.js
@@ -0,0 +1,1 @@
+import"./index.49303299.js";import"./slicedToArray.f13a096f.js";import"./asyncToGenerator.158bdab7.js";import{s as r}from"./app.0084af3f.js";r({target:document.querySelector("#sapper")});
