e7131671930c6880e5ba9a6a86d7ba07c87e9749
Le Liam <nghenglim@gmail.com>
2020-01-02 17:52:59 +0800

    fix

---
+++ client/client.fce05c27.js
@@ -0,0 +1,1 @@
+import"./index.782a6ae6.js";import{s as e}from"./app.cb0df2a4.js";e({target:document.querySelector("#sapper")});
---
+++ client/legacy/client.ce42430a.js
@@ -0,0 +1,1 @@
+import"./index.d1f462d0.js";import"./slicedToArray.f13a096f.js";import"./asyncToGenerator.eec0292a.js";import{s as r}from"./app.b808d755.js";r({target:document.querySelector("#sapper")});
