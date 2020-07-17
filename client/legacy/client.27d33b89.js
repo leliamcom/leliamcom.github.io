985bfa1560dad059c7f24a6aaeacb3ec4bcc4abe
nghenglim <me@leliam.com>
2020-07-17 17:01:13 +0800

    update and add privacy policy

---
+++ client/legacy/client.27d33b89.js
@@ -0,0 +1,1 @@
+import"./index.50824c64.js";import"./slicedToArray.f13a096f.js";import"./asyncToGenerator.982d467e.js";import{s as r}from"./app.8a290f56.js";import"./Nav.61444e53.js";r({target:document.querySelector("#sapper")});
