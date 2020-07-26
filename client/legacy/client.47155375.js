c0c333f2f88e784e0587e237c098b545095b4a95
nghenglim <me@leliam.com>
2020-07-26 09:07:37 +0800

    update

--- client/legacy/client.47155375.js
+++
@@ -1,1 +0,0 @@
-import"./index.50824c64.js";import"./slicedToArray.f13a096f.js";import"./asyncToGenerator.982d467e.js";import{s as e}from"./app.a1bafeeb.js";import"./Nav.61444e53.js";e({target:document.querySelector("#sapper")});
